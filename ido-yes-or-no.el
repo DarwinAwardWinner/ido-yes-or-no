8953eadaaa7811ebc66d8a9eb7ac43f38913ab59
Ryan C. Thompson <rct@thompsonclan.org>
2016-11-08 15:51:24 -0800

    Add a proper header to the lisp file.

--- ido-yes-or-no.el
+++ ido-yes-or-no.el
@@ -1,11 +1,45 @@
 ;;; ido-yes-or-no.el --- Use Ido to answer yes-or-no questions
 
-;; Copyright 2011 Ryan C. Thompson
+;; Copyright (C) 2011 Ryan C. Thompson
 
+;; Filename: ido-yes-or-no.el
 ;; Author: Ryan C. Thompson
 ;; URL: https://github.com/DarwinAwardWinner/ido-yes-or-no
 ;; Version: 1.4
+;; Created: 2011-09-24
 ;; Package-Requires: ((ido-completing-read+ "0"))
+;; Keywords: convenience, completion, ido
+
+;; This file is NOT part of GNU Emacs.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+
+;; This package lets you use ido to answer yes-or-no questions. This
+;; gives you a middle ground between `yes-or-no-p', which requires you
+;; to type out the full word, and `y-or-n-p', which is too easy to
+;; unintentionally answer with a single keystroke. To use this
+;; package, enable `ido-yes-or-no-mode'.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
 
 (require 'ido)
 
